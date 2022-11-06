c26294ed42d63c0aa584dca0782dedfcdefae033
isacpxc <isacpxc@gmail.com>
2022-11-06 02:14:41 -0300

    movement and boundaries added, static yet

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
@@ -42,24 +43,69 @@ int main()
 
         }
     }
-    p_Area[10][20] = '0';
+    int p_x=n;
+    int p_y=floor(m/2);
 
+    p_Area[p_y][p_x] = '0';
 
 
 //    printf("\n\n\n\n");
 
 
 printf("=======PRESS ENTER TO START=======");
-while(getch() != 'e'){
+
+
+char input=' ';
+while(input != 'e'){
+    input=getch();
+//    printf("%c",input);
+//    system("pause");
     system("cls");
+
+    switch(input)
+    {
+    case 'w':
+        if (p_y != 1){
+            p_Area[p_y][p_x] = ' ';
+            p_y--;
+        }
+        break;
+    case 's':
+        if (p_y != m-2){
+            p_Area[p_y][p_x] = ' ';
+            p_y++;
+        }
+        break;
+    case 'a':
+        if (p_x != 1){
+            p_Area[p_y][p_x] = ' ';
+            p_x--;
+        }
+        break;
+    case 'd':
+        if (p_x != n*2-3){
+            p_Area[p_y][p_x] = ' ';
+            p_x++;
+        }
+        break;
+    default:
+        break;
+    }
+    p_Area[p_y][p_x] = '0';
+
+
+
     for (size_t i = 0; i<m ; i++){
         for (size_t j = 0; j<(n*2) ; j++){
             printf("%c", p_Area[i][j]);
         }
     }
+
 }
 
 
+
+printf("FIM");
 
 
 
